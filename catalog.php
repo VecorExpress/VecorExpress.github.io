9b974bdb1dfb8ff31d2371b75f935227e441f410
VecorExpress <maflores@americanschooltrujillo.edu.pe>
2024-11-15 17:42:55 -0500

    Add files via upload

---
+++ catalog.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: #f4f4f9; text-align: center; padding: 50px; font-family: 'Roboto', sans-serif;">
+    <h1 style="color: #333; font-size: 24px; margin-bottom: 20px;">
+        Parece que se nos acabaron los ladrillos para terminar el catálogo. <br> 
+        Si no quieres que te pase, pide tus ladrillos usando la web de Vecor<br>
+        Pronto terminaremos esta parte... se paciente.
+    </h1>
+    <button type="button" style="background-color: #113896; color: #ffc300; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+        <a href="index.php" style="color: white; text-decoration: none;">Regresar</a>
+    </button>
+</body>
+</html>
